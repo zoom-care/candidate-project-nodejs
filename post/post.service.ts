a0ba719c09e60f0824f838055c8c9e4324b7bf74
Alex Luecke <github19@xndr.me>
2019-06-03 21:33:15 -0700

    give ability to query for posts with comments

--- post/post.service.ts
+++ post/post.service.ts
@@ -1,15 +1,19 @@
 import Loki from 'lokijs';
 
 import { Post } from './post';
+import { Comment } from '../comment/comment';
 
 export class PostService {
-  private readonly db: Loki;
-
-  constructor(db: Readonly<Loki>) {
+  constructor(private db: Readonly<Loki>) {
     this.db = db;
   }
 
-  public getData(): Post[] {
-    return this.db.getCollection<Post>('posts').data;
+  public getData(whereFn: (post: Post) => boolean = _ => true): (Post & { comment: Comment })[] {
+    return this.db.getCollection('posts').where(whereFn).map(post => {
+      return {
+        ...post,
+        comments: this.db.getCollection<Comment>('comments').where(comment => comment.postId === post.id)
+      };
+    });
   }
 }
