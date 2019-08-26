da8e26166a22287f6aad4470c3a15db490479a44
Aaron Eiche <aaron@aaroneiche.com>
2019-08-25 22:46:36 -0700

    Adds some validation to the User creation API.

--- api/users.js
+++ api/users.js
@@ -4,19 +4,43 @@ var auth = require('./authorization');
 
 var db = require('../config/loki').getDatabase();
 
+let getUsers = (req, res, next) => {
+  req.users = db.getCollection('users');
+  next();
+}
+
 //This is not strictly required for the challenge, but is a good starting point.
 //return all users
-router.get('/', (req,res,next) => {
-  var users = db.getCollection('users');
-  res.json(users.data);
+router.get('/', getUsers ,(req,res,next) => {
+  res.json(req.users.data);
 })
 
-router.post('/', auth ,(req,res,next)=>{
-  var users = db.getCollection('users');
-  users.on('insert', input=>{ input.id = input.$loki; });
-  let newUser = users.insert(req.body);
-  
+router.post('/', auth, getUsers,(req,res,next)=>{
+  req.users.on('insert', input=>{ input.id = input.$loki; });
 
+  //Some limited validation
+  let requiredProps = ['name','username','email'];
+  requiredProps.forEach(term=>{
+    if(req.body[term] == undefined ||
+       req.body[term].length == 0)  {
+         res.status(400);
+         res.send("One or more missing required fields.");
+         return;
+       }
+  })
+
+  //also, lets make sure we're not duplicating.
+  let emailTest = req.users.findOne({"email":String(req.body.email)});
+  let usernameTest = req.users.findOne({"email":String(req.body.username)});
+  console.log(emailTest,usernameTest);
+  if(emailTest != null || usernameTest != null) {
+    res.status(400);
+    res.send("Email or Username already taken");
+    return;
+  }
+
+  let newUser = req.users.insert(req.body);
+  
   res.status("201");
   res.send(`/api/users/${newUser.$loki}`);
 })
