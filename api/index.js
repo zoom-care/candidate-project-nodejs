28ac86294fc7736f722d4a7056f6c132083c7e8e
Aaron Eiche <aaron@aaroneiche.com>
2019-08-25 22:07:03 -0700

    Allows CORS through API endpoint.

--- api/index.js
+++ api/index.js
@@ -5,6 +5,14 @@ var posts = require("./posts");
 var comments = require("./comments");
 
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  next();
+});
+
 router.use('/users', users);
 router.use('/posts', posts);
 router.use('/comments', comments);
