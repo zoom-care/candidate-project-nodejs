a2f8e1f257adaf81d4e186cd386a07972c1e25e7
1jerry <git-73ry@sneakemail.com>
2020-03-05 10:16:26 -0800

    add posts, list and get

--- api/index.js
+++ api/index.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const config = require('../config/loki');
-config.init();
-console.log('init');
-const db = config.getDatabase();
+const express = require('express')
+const config = require('../config/loki')
+config.init()
+console.log('init database')
+const db = config.getDatabase()
 
-const router = express.Router();
-const users = db.getCollection('users');
+const router = express.Router()
+const users = db.getCollection('users')
+const posts = db.getCollection('posts')
+const comments = db.getCollection('comments')
 
 router.param('user_id', (req, res, next, id) => {
-    req.user = users.get(id);
-    next();
+    req.user = users.get(id)
+    if (!req.user) res.status(404)
+    next()
+})
+
+router.param('post_id', (req, res, next, id) => {
+    req.post = posts.get(id)
+    if (!(req.post && req.post.userId === req.user.id)) {
+        res.status(404)
+        req.post = null
+    }
+    next()
 })
 
 router.route('/')
@@ -19,7 +31,7 @@ router.route('/')
             length: users && users.data && users.data.length || "0",
             data: users && users.data || {}
         })
-    });
+    })
 
 router.route('/:user_id')
     .get((req, res, next) => {
@@ -33,7 +45,31 @@ router.route('/:user_id')
     })
     .delete((req, res, next) => {
         next(new Error('not implemented'))
-    });
+    })
+
+router.route('/:user_id/posts')
+    .get((req, res, next) => {
+        let result = posts.find({userId:req.user.id})
+        res.status(200).json({
+            message: "Posts listed",
+            length: result.length || "0",
+            data: result || {}
+        })
+    })
+
+router.route('/:user_id/posts/:post_id')
+    .get((req, res, next) => {
+        res.json(req.post)
+    })
+    .put((req, res, next) => {
+        next(new Error('not implemented'))
+    })
+    .post((req, res, next) => {
+        next(new Error('not implemented'))
+    })
+    .delete((req, res, next) => {
+        next(new Error('not implemented'))
+    })
 
 
-module.exports = router;
+module.exports = router
