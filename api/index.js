f3eb283ddb588e9442614c52a6200b1e0d7425af
1jerry <git-73ry@sneakemail.com>
2020-03-05 09:10:47 -0800

    basic user get action

--- api/index.js
+++ api/index.js
@@ -1,14 +1,39 @@
 const express = require('express');
+const config = require('../config/loki');
+config.init();
+console.log('init');
+const db = config.getDatabase();
 
 const router = express.Router();
+const users = db.getCollection('users');
+
+router.param('user_id', (req, res, next, id) => {
+    req.user = users.get(id);
+    next();
+})
 
 router.route('/')
     .get((req, res, next) => {
         res.status(200).json({
             message: "Users listed",
-            data: {}
+            length: users && users.data && users.data.length || "0",
+            data: users && users.data || {}
         })
-        }
-    );
+    });
+
+router.route('/:user_id')
+    .get((req, res, next) => {
+        res.json(req.user)
+    })
+    .put((req, res, next) => {
+        next(new Error('not implemented'))
+    })
+    .post((req, res, next) => {
+        next(new Error('not implemented'))
+    })
+    .delete((req, res, next) => {
+        next(new Error('not implemented'))
+    });
+
 
 module.exports = router;
