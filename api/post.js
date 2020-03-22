914133fec756e624c8b61fc364a09f36a6a32bb3
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2020-03-21 18:12:03 -0700

    add validation for post and patch endpoints

--- api/post.js
+++ api/post.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser');
+const { body, validationResult, matchedData } = require('express-validator');
 const auth = require('../lib/auth');
 const db = require('../config/loki').getDatabase();
 
@@ -12,8 +13,19 @@ function postRoutes(api) {
         '/post/:postId',
         auth,
         bodyParser.json(),
+        [
+            body('title').optional().isString(),
+            body('body').optional().isString()
+        ],
         (req, res) => {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).send({ errors: errors.array() });
+            }
+
+            const updates = matchedData(req);
+
             const posts = db.getCollection('posts');
             const postId = parseInt(req.params.postId);
 
@@ -22,8 +34,8 @@ function postRoutes(api) {
                 return res.status(404).send('post not found');
             }
 
-            Object.keys(req.body).forEach(key => {
-                post[key] = req.body[key];
+            Object.keys(updates).forEach(key => {
+                post[key] = updates[key];
             });
 
             posts.update(post);
--- api/user.js
+++ api/user.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser');
+const { body, validationResult, matchedData } = require('express-validator');
 const auth = require('../lib/auth');
 const db = require('../config/loki').getDatabase();
 
@@ -12,10 +13,27 @@ function userRoutes(api) {
         '/user',
         auth,
         bodyParser.json(),
+        [
+            body('name').isString().not().isEmpty(),
+            body('username').isString().not().isEmpty(),
+            body('email').isEmail(),
+            body('address').optional(),
+            body('phoneNumbers').optional().isArray(),
+            body('website').optional()
+        ],
         (req, res) => {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).send({ errors: errors.array() });
+            }
+
+            const data = matchedData(req);
+
             const users = db.getCollection('users');
-            users.insert(req.body);
+
+            users.insert(data);
+
             return res.status(201).send();
         } catch (err) {
             console.error(err);
