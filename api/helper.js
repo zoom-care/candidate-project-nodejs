520c6243dc795dcd4172b325b12a9705370f5d19
Kevan Simpson <kevan.simpson@gmail.com>
2020-03-23 09:20:25 -0700

    refactor auth check and helpers

--- api/helper.js
+++ api/helper.js
@@ -1,25 +1,15 @@
 var createError = require('http-errors');
+var logger = require('../logger');
 var dataService = require('./dataService');
 
 module.exports = {
-  missingAuthHeader (req) {
-    return !(req.header('Authorization') || req.header('authorization'))
-  },
+  checkAuthHeader (req, res, next) {
+    // don't require auth for reads
+    if (/get/i.test(req.method)) next();
 
-  // look for user's post, hand to callback or return 404
-  postAction(req, res, callback) {
-    const userId = Number(req.params.userId);
-    const postId = Number(req.params.postId);
-    const post = dataService.getPost(userId, postId);
-    if (post) return callback(res, post);
-    else res.status(404).json(createError(404, `Post ${postId} by ${userId} does not exist!`));
-  },
-
-  // look for user, hand to callback or return 404
-  userAction(req, res, callback) {
-    const userId = Number(req.params.userId);
-    const user = dataService.getUser(userId);
-    if (user) return callback(res, user);
-    else res.status(404).json(createError(404, `User ${userId} does not exist!`));
+    if (!(req.header('Authorization') || req.header('authorization'))) {
+      logger.warn('Unauthorized access attempted!');
+      res.status(401).json(createError(401, 'Not Authorized'));
+    } else next();
   }
 }
--- api/postRouter.js
+++ api/postRouter.js
@@ -1,46 +1,47 @@
-var createError = require('http-errors');
 var express = require('express');
 var dataService = require('./dataService');
 var helpers = require('./helper')
+
+// look for user's post, hand to callback or return 404
+const postAction = (req, res, callback) => {
+  const userId = Number(req.params.userId);
+  const postId = Number(req.params.postId);
+  const post = dataService.getPost(userId, postId);
+  if (post) return callback(res, post);
+  else res.status(404).json(createError(404, `Post ${postId} by ${userId} does not exist!`));
+};
 
 // you need to set mergeParams: true on the router,
 // if you want to access params from the parent router
 var postRouter = express.Router({mergeParams: true});
 
+postRouter.all('*', (req, res, next) => {
+  helpers.checkAuthHeader(req, res, next);
+});
+
 postRouter.route('/')
   .get((req, res) => {
-    console.log(`FOO => ${req.params.userId}`);
     res.status(200).send(dataService.getAllPosts(req.params.userId));
   })
   .post((req, res) => { // create post
-    if (helpers.missingAuthHeader(req)) {
-      res.status(401).json(createError(401, 'Not Authorized'));
-    } else res.status(201).send(dataService.createPost(req.body));
+    res.status(201).send(dataService.createPost(req.body));
   });
 
 postRouter.route('/:postId')
   .delete((req, res) => { // delete post
-    if (helpers.missingAuthHeader(req)) {
-      res.status(401).json(createError(401, 'Not Authorized'));
-    } else {
-      helpers.postAction(req, res, (response, post) => {
-        response.status(200).send(dataService.removePost(post));
-      });
-    }
+    postAction(req, res, (response, post) => {
+      response.status(200).send(dataService.removePost(post));
+    });
   })
   .get((req, res) => { // find post
-    helpers.postAction(req, res, (response, post) => {
+    postAction(req, res, (response, post) => {
       response.status(200).send(post);
     });
   })
   .put((req, res) => { // update post
-    if (helpers.missingAuthHeader(req)) {
-      res.status(401).json(createError(401, 'Not Authorized'));
-    } else {
-      helpers.postAction(req, res, (response, post) => {
-        response.status(200).send(dataService.savePost(Object.assign(post, req.body)));
-      });
-    }
+    postAction(req, res, (response, post) => {
+      response.status(200).send(dataService.savePost(Object.assign(post, req.body)));
+    });
   });
 
 module.exports = postRouter;
